e2511da61666d248122e57308569ca39aca98ed7
TopNik073 <borzovn169@gmail.com>
2025-02-02 21:52:25 +0200

    Enable postgres trigram extension

---
+++ migrations/versions/2025_02_02_2039-b26aa53191da_add_trigram_extension.py
@@ -0,0 +1,26 @@
+"""Add trigram extension
+
+Revision ID: b26aa53191da
+Revises:
+Create Date: 2025-02-02 20:39:41.066606
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "b26aa53191da"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade():
+    op.execute('CREATE EXTENSION IF NOT EXISTS pg_trgm')
+
+def downgrade():
+    op.execute('DROP EXTENSION pg_trgm') 
