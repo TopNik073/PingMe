8f744cd5dd882a5da5dad48f269f9be16c9c11a4
TopNik073 <borzovn169@gmail.com>
2025-02-11 00:27:29 +0200

    Connect Auth router with app
    Connect Auth router with app and init the Redis connection

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from contextlib import asynccontextmanager
+from sys import prefix
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -6,22 +8,25 @@ from datetime import datetime, timezone
 
 from src.core.config import settings
 from src.infrastructure.database.session import engine
+from src.infrastructure.cache.redis.connection import init_redis_pool, close_redis_pool
+from src.presentation.api import auth_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # Startup
-    # Здесь инициализируем подключения к БД, Redis и т.д.
+    # Инициализируем подключения к БД и Redis
+    app.state.redis = await init_redis_pool()
+
     yield
+
     # Shutdown
+    # Закрываем подключения
+    await close_redis_pool(app.state.redis)
     await engine.dispose()
 
 
-app = FastAPI(
-    title=settings.APP_NAME,
-    debug=settings.DEBUG,
-    lifespan=lifespan
-)
+app = FastAPI(title=settings.APP_NAME, debug=settings.DEBUG, lifespan=lifespan, prefix="api/v1")
 
 # CORS middleware
 app.add_middleware(
@@ -33,21 +38,22 @@ app.add_middleware(
 )
 
 # Здесь будем подключать роутеры
-# app.include_router(auth_router)
+app.include_router(auth_router)
 # app.include_router(messages_router)
 # и т.д.
 
 
+@app.get("/")
 @app.get("/health")
 async def health_check():
-    return {"status": "ok", "timestamp": datetime.now(timezone.utc)}
+    return {
+        "status": "ok",
+        "timestamp": datetime.now(timezone.utc),
+        "redis": await app.state.redis.ping(),  # Проверяем подключение к Redis
+    }
 
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(
-        "main:app",
-        host="0.0.0.0",
-        port=8000,
-        reload=True
-    ) 
+
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)
---
+++ src/presentation/api/__init__.py
@@ -0,0 +1,5 @@
+from src.presentation.api.v1.auth.router import router as auth_router
+
+__all__ = [
+    auth_router
+]
